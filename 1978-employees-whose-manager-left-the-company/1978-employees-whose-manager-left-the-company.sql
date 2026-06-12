8b28d29e20d1c1a9495dbce1eae9b3aeed341227
SARAVANAN2308-R <sara23vanan05@gmail.com>
2026-06-12 12:13:05 +0530

    Time: 393 ms (14.95%), Space: 0B (100%) - LeetHub

--- 1978-employees-whose-manager-left-the-company/1978-employees-whose-manager-left-the-company.sql
+++ 1978-employees-whose-manager-left-the-company/1978-employees-whose-manager-left-the-company.sql
@@ -1,8 +1,7 @@
-SELECT e.employee_id
-FROM employees e
-LEFT JOIN employees m
-ON e.manager_id = m.employee_id
-WHERE e.salary < 30000
-  AND e.manager_id IS NOT NULL
-  AND m.employee_id IS NULL
-ORDER BY e.employee_id;
+select employee_id
+from employees
+where salary<30000
+and manager_id not in(
+    select employee_id from employees
+)
+order by employee_id;
