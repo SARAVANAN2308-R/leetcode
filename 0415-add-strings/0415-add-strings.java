63ede3ccfd93cbab480692fed953b36b17eb1225
SARAVANAN2308-R <sara23vanan05@gmail.com>
2026-08-28 09:57:06 +0530

    Time: 9 ms (8.51%), Space: 47.3 MB (5.28%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String addStrings(String num1, String num2) {
+
+        int i = num1.length() - 1;
+        int j = num2.length() - 1;
+
+        int carry = 0;
+        String str = "";
+        while (i >= 0 || j >= 0) {
+            int sum = carry;
+            if (i >= 0) {
+                sum += num1.charAt(i) - '0';
+                i--;
+            }
+            if (j >= 0) {
+                sum += num2.charAt(j) - '0';
+                j--;
+            }
+            str += (sum % 10);
+            carry = sum / 10;
+        }
+        if (carry > 0) {
+            str += carry;
+        }
+        return new StringBuilder(str).reverse().toString();
+    }
+}
