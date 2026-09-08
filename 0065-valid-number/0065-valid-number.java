1dc598fa7c17f35f3434398adc66c9f5f9822ed7
SARAVANAN2308-R <sara23vanan05@gmail.com>
2026-09-08 14:35:06 +0530

    Time: 1 ms (100%), Space: 43.7 MB (92.36%) - LeetHub

---
+++ 0065-valid-number/0065-valid-number.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean isNumber(String s) {
+        boolean dotseen=false;
+        boolean eseen=false;
+        boolean digitseen=false;
+        boolean digitae=true;
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(Character.isDigit(ch)){
+                digitseen=true;
+                if(eseen){
+                    digitae=true;
+                }
+            }else if(ch=='+' ||ch=='-'){
+                   if(i!=0 && s.charAt(i-1)!='e' && s.charAt(i-1)!='E'){
+                    return false;
+                   }
+            }else if(ch=='.'){
+                if(dotseen||eseen){
+                    return false;
+                }
+                dotseen=true;
+            }else if(ch=='e' || ch=='E'){
+                if(eseen||!digitseen){
+                    return false;
+                }
+                eseen=true;
+                digitae=false;
+            }else{
+                return false;
+            }
+        }
+        return digitseen&&digitae;
+    }
+}
